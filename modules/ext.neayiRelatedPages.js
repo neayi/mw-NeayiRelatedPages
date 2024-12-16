e8ab1896f17efafda23093c285ff4f7ba909f29a
Bertrand Gorge <bertrand.gorge@neayi.com>
2024-12-16 12:24:47 +0100

    Added a way to translate categories in plural

--- modules/ext.neayiRelatedPages.js
+++ modules/ext.neayiRelatedPages.js
@@ -7,77 +7,100 @@ var NeayiRelatedPages_controller = ( function () {
 
 			console.log('Neayi Related Pages');
 			
-			// $(".neayi-related-pages")
 			var api = new mw.Api();
+
+			//https://wiki.dev.tripleperformance.fr/api.php?action=expandtemplates&text=%7B%7BTranslationArray%7D%7D&prop=wikitext
 			api.get( {
-				'action': 'query',
-				'prop': 'relatedpages',
-				'titles': mw.config.get('wgTitle')
+				'action': 'expandtemplates',
+				'prop': 'wikitext',
+				'text': '{{TranslationArray}}'
 			} )
 			.done( function ( data ) {
+				let categories = JSON.parse(data.expandtemplates.wikitext);
+				console.log(categories);
 
-				console.log(data);
-
-				let relatedPagesDiv = $($(".neayi-related-pages")[0]);
-				let pages = Object.values(data.query.pages);
-
-				let pageTypes = {};
-
-				pages.forEach(aPage => {
-					if (!aPage['relatedpages'])
-						return;
-
-					let pageType = aPage['relatedpages']['A un type de page'];
-
-					pageType.forEach(aType => {
-						if (pageTypes[aType] == undefined)
-							pageTypes[aType] = new Array();
-
-						pageTypes[aType].push(aPage['relatedpages']);
+				api.get( {
+					'action': 'query',
+					'prop': 'relatedpages',
+					'titles': mw.config.get('wgTitle')
+				} )
+				.done( function ( data ) {
+	
+					console.log(data);
+	
+					let relatedPagesDiv = $($(".neayi-related-pages")[0]);
+					let pages = Object.values(data.query.pages);
+	
+					let pageTypes = {};
+	
+					pages.forEach(aPage => {
+						if (!aPage['relatedpages'])
+							return;
+	
+						let pageType = aPage['relatedpages']['A un type de page'];
+	
+						pageType.forEach(aType => {
+							if (pageTypes[aType] == undefined)
+								pageTypes[aType] = new Array();
+	
+							pageTypes[aType].push(aPage['relatedpages']);
+						});
+	
 					});
-
-				});
-
-				Object.keys(pageTypes).forEach(aType => {
-					let pages = pageTypes[aType];
-					
-					let searchURL = "/index.php?title=Search&filters=A+un+type+de+page%5E%5E"+encodeURIComponent(aType)+"&term=" + encodeURIComponent(mw.config.get("wgPageName"));
-					let html = `<div class="row navigation-not-searchable searchaux smw-no-index my-1">
-							<div class="col-md-8 text-md-left text-center align-self-center"><h3 class="m-0"><span class="mw-headline">${aType}</span></h3></div>
-							<div class="col-md-4 text-md-right text-center align-self-center"><span class="btn btn-dark-green text-nowrap furtherresults"><a href="${searchURL}" title="Search">${mw.msg('neayirelatedpages-seeall', pages.length)}</a></span></div>
-						</div>
-						<div class="row portal-store-results searchaux">`
-
-					pages.slice(0, 6).forEach(aPage => {
-						let title = aPage.Title;
-						let imageURL = aPage.ImageURL;
-						let URL = aPage.URL;
 	
-						let imageNode = '';
-						if (imageURL.length > 0) {
-							imageNode = `<a href="/wiki/${URL}"
-								title="${title}"><img src="${imageURL}" decoding="async" class="card-img""></a>`;
-						}
+					categories.forEach(aCat => {
+						let aType = aCat.singular;
+						let aTypePlural = aCat.plural;
+
+						if (pageTypes[aType] == undefined)
+							return;
+
+						let pages = pageTypes[aType];
+						
+						let searchURL = "/index.php?title=Search&filters=A+un+type+de+page%5E%5E"+encodeURIComponent(aType)+"&term=" + encodeURIComponent(mw.config.get("wgPageName"));
+						let html = `<div class="row navigation-not-searchable searchaux smw-no-index my-1">
+								<div class="col-md-8 text-md-left text-center align-self-center"><h3 class="m-0"><span class="mw-headline">${aTypePlural}</span></h3></div>
+								<div class="col-md-4 text-md-right text-center align-self-center"><span class="btn btn-dark-green text-nowrap furtherresults"><a href="${searchURL}" title="Search">${mw.msg('neayirelatedpages-seeall', pages.length)}</a></span></div>
+							</div>
+							<div class="row portal-store-results searchaux">`
 	
-						html += `<div class="col-xl-6 mb-lg-0 mb-3">
-				<div class="very-small-card card mb-3">
-					<div class="row no-gutters">
-						<div class="col-md-3 image-col">${imageNode}</div>
-						<div class="col-md-9">
-							<div class="card-body px-2 py-1">
-								<p class="mb-0"><span class="badge badge-light">${aType}</span></p>
-								<p class="card-text"><a class="stretched-link" href="/wiki/${URL}">${title}</a></p>
+						pages.slice(0, 6).forEach(aPage => {
+							let title = aPage.Title;
+							let imageURL = aPage.ImageURL;
+							let URL = aPage.URL;
+		
+							let imageNode = '';
+							if (imageURL.length > 0) {
+								imageNode = `<a href="/wiki/${URL}"
+									title="${title}"><img src="${imageURL}" decoding="async" class="card-img""></a>`;
+							}
+		
+							html += `<div class="col-xl-6 mb-lg-0 mb-3">
+					<div class="very-small-card card mb-3">
+						<div class="row no-gutters">
+							<div class="col-md-3 image-col">${imageNode}</div>
+							<div class="col-md-9">
+								<div class="card-body px-2 py-1">
+									<p class="mb-0"><span class="badge badge-light">${aType}</span></p>
+									<p class="card-text"><a class="stretched-link" href="/wiki/${URL}">${title}</a></p>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</div>`;
-					});		
-					
-					html += '</div>';
-					relatedPagesDiv.append(html)
+				</div>`;
+						});		
+						
+						html += '</div>';
+						relatedPagesDiv.append(html)
+
+					});
+
+					Object.keys(pageTypes).forEach(aType => {
+						
+					});
 				});
 			});
+
 		}
 	};
 }());
